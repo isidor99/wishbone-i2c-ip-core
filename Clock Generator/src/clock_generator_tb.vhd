74bea21b93445974acac4d0a6d53be4e4491a2f4
Aleksandar Beslic <aleksandar.beslic@student.etf.unibl.org>
2022-01-18 22:38:41 +0100

    Issue #2: Create test bench for clock generator
    Modifying testbench to follow the coding standard

--- Clock Generator/src/clock_generator_tb.vhd
+++ Clock Generator/src/clock_generator_tb.vhd
@@ -31,19 +31,19 @@ architecture arch of clock_generator_tb is
       g_SYSTEM_CLOCK : integer
     );
     port(
-	   clk_i : in  std_logic;
+      clk_i : in  std_logic;
       rst_i : in  std_logic;
       enb_i : in  std_logic;
       sel_i : in  std_logic_vector(1 downto 0);
       clk_o : out std_logic
     );
   end component;
-  
+
   constant c_T              : time    := 20 ns;
   constant c_STD_MODE       : time    := 10000 ns;
   constant c_FAST_MODE      : time    := 2500 ns;
   constant c_FAST_MODE_PLUS : time    := 1000 ns;
-  constant SYSTEM_CLOCK     : natural := 50_000_000;
+  constant c_SYSTEM_CLOCK     : natural := 50_000_000;
 
   signal clk     : std_logic;
   signal rst     : std_logic;
@@ -53,14 +53,14 @@ architecture arch of clock_generator_tb is
 begin
 
   -- uut instantiation
-  uut: clock_generator
-    generic map(g_SYSTEM_CLOCK => SYSTEM_CLOCK)
-	 port map(
-	   clk_i => clk,
-		rst_i => rst,
-		enb_i => enbl,
-		sel_i => sel,
-		clk_o => clk_out);
+  uut : clock_generator
+    generic map(g_SYSTEM_CLOCK => c_SYSTEM_CLOCK)
+      port map(
+        clk_i => clk,
+        rst_i => rst,
+        enb_i => enbl,
+        sel_i => sel,
+        clk_o => clk_out);
 
   -- clock generator
   process
@@ -75,47 +75,47 @@ begin
   process
   begin
     rst <= '1';
-	 wait for c_T;
-	 rst <= '0';
-	 wait for c_T;
+    wait for c_T;
+    rst <= '0';
+    wait for c_T;
 
-	 -- Checking output in the inactive state
-	 enbl <= '0';
-	 wait for c_T;
-	 
-	 assert(clk_out = '1')
-	   report "Output should be high in the inactive state " & std_logic'image(clk_out)
-	     severity error;
+    -- Checking output in the inactive state
+    enbl <= '0';
+    wait for c_T;
+
+    assert(clk_out = '1')
+      report "Output should be high in the inactive state " & std_logic'image(clk_out)
+        severity error;
 
     -- Enabling output
-	 enbl <= '1';
-	 
+    enbl <= '1';
+
     -- Standar mode testing
-	 sel <= "00";
-	 wait until rising_edge(clk_out);
-	 wait for c_STD_MODE + c_T;
+    sel <= "00";
+    wait until rising_edge(clk_out);
+    wait for c_STD_MODE + c_T;
 
-	 assert(clk_out = '1')
-	   report "Output should be high but it's " & std_logic'image(clk_out)
-	     severity error;
-	 
-	 -- Fast mode testing	  
+    assert(clk_out = '1')
+      report "Output should be high but it's " & std_logic'image(clk_out)
+        severity error;
+
+    -- Fast mode testing
     sel <= "01";
-	 wait until rising_edge(clk_out);
-	 wait for c_FAST_MODE + c_T;
+    wait until rising_edge(clk_out);
+    wait for c_FAST_MODE + c_T;
 
-	 assert(clk_out = '1')
-	   report "Output should be high but it's " & std_logic'image(clk_out)
-	     severity error;
-	
-	 -- Fast mode plus testing	  
+    assert(clk_out = '1')
+      report "Output should be high but it's " & std_logic'image(clk_out)
+        severity error;
+
+    -- Fast mode plus testing
     sel <= "10";
-	 wait until rising_edge(clk_out);
-	 wait for c_FAST_MODE_PLUS + c_T;
+    wait until rising_edge(clk_out);
+    wait for c_FAST_MODE_PLUS + c_T;
 
-	 assert(clk_out = '1')
-	   report "Output should be high but it's " & std_logic'image(clk_out)
-	     severity error;
-	 wait;
+    assert(clk_out = '1')
+      report "Output should be high but it's " & std_logic'image(clk_out)
+        severity error;
+    wait;
   end process;
 end arch;
